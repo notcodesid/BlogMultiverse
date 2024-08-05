cd57f8d2517420daf52fe9571d366ef47c055bde
siddharth <notcodesid@gmail.com>
2024-08-06 01:01:20 +0530

    a bit of change in bulk route & cors add

--- server/src/routes/blog.ts
+++ server/src/routes/blog.ts
@@ -96,7 +96,18 @@ blogRouter.get('/bulk', async (c) => {
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
-    const blogs = await prisma.post.findMany({});
+    const blogs = await prisma.post.findMany({
+        select: {
+            content: true,
+            title: true,
+            id: true,
+            author: {
+                select: {
+                    username: true
+                }
+            }
+        }
+    });
 
     return c.json({
         blogs
